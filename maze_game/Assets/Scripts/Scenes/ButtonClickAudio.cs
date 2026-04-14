e1e3371edcbe3c03b7fe8cc09737d2e1c06ae317
Rustem Maldybekov <mrustem501@gmail.com>
2026-04-14 22:56:30 +0600

    Added Background Audio

---
+++ maze_game/Assets/Scripts/Scenes/ButtonClickAudio.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ButtonClickAudio : AbstractActionButton
+{
+    [SerializeField] protected AudioClip audioClip;
+
+    protected override void OnClick()
+    {
+    }
+}
